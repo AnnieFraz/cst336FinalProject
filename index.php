c9bacfaee31e9b42ad1ed40738f33e6b5d2f1565
Anna Rasburn <annarasburn97@gmail.com>
2017-12-03 20:15:54 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+?>
+<html>
+<head>
+        <meta charset="utf-8"/>
+        <link rel="shortcut icon" href="img/logo.jpg" type="image/png">
+        <title>SU Live Music Society</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        
+        <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>Stirling University Live Music Society</h1>
+        </header>
+        <nav>
+            <hr width="50%"/>
+            <a href="index.php"><strong>Home</strong></a>
+            <a href="about.php">About</a>
+            <a href="events.php">Events</a>
+            <a href="roomBooking.php">Room Booking</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+        
+        <br/><br />
+        <main>
+            
+            
+            <embed width="620" height="420"
+src="video/promo.mp4">
+
+     <center>      
+<div id="welcomeText" style="width:80%;text-align: center;">
+<p><strong>Are you musically inclined? Keen to get involved in the Stirling live music scene? Looking for an excuse to bring your instrument to university?</strong></p>
+<p>Look no further, the Live Music Society is here to provide you with opportunities to get involved in open mics, to collaborate with fellow musicians, and to give you a place to come for equipment, instruments, and information about local music and potential gigs.</p>
+<p>No matter what instrument you play, or what genres you enjoy, Live Music has a space for you, both metaphorically and physically. Our society has access to a cottage on campus where we keep our society drum kit, keyboard, and PA system, and which you can book out to use as a practice room for yourself or your future band!</p>
+<p>We want to expand the society and make it as inclusive as possible, so we are very open to ideas for expansion. We hope to create sub groups for different types of musicians, and also to provide more opportunities for members to get involved in playing music in the wider Stirling community. Our aim is that our members will look to us as a social group where they can connect with likeminded people with similar interests while also putting their musical talents to good use.</p>
+<p>Join our society and we will work with you to create a space for all musicians at the University of Stirling to come together and create music!</p>
+</div>
+</center>
+        </main>
+        
+        </body>
+        
+         <footer>
+            <br></br>
+            <a href="https://twitter.com/SULiveMusic" style="color:white; font-size:25px;"><i class="fa fa-twitter"></i></a>
+            <a href="https://www.facebook.com/StirlingLiveMusic/" style="color:white; font-size:25px;"><i class="fa fa-facebook"></i></a>
+            <a href="https://www.instagram.com/stirlinglivemusic/?hl=en"style="color:white; font-size:25px;"><i class="fa fa-instagram"></i></a>
+            
+            <h3>Live Music Society - University of Stirling</h3>
+            <br></br>
+        </footer>
+        
+</html>
